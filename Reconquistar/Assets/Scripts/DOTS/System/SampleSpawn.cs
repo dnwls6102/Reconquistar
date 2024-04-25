85eec74925b3f1d82ece482b1763620c98c0408d
fjaoief <darthgun@naver.com>
2024-04-26 03:26:05 +0900

    no message

--- Reconquistar/Assets/Scripts/DOTS/System/SampleSpawn.cs
+++ Reconquistar/Assets/Scripts/DOTS/System/SampleSpawn.cs
@@ -44,7 +44,7 @@ namespace _1.Scripts.DOTS.System
                 {
                     index = new int2(x, y),
                     hp = 3,
-                    movementspeed = 1f,
+                    movementspeed = 0.01f,
                     dmg = 1,
                     team = newteam
                 });
