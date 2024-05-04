1fa57e49eba4ec54ab2ad28e0c42a24e8e42442e
fjaoief <darthgun@naver.com>
2024-05-04 17:54:16 +0900

    dependency 수정

--- Reconquistar/Assets/Scripts/DOTS/System/SampleSpawn.cs
+++ Reconquistar/Assets/Scripts/DOTS/System/SampleSpawn.cs
@@ -14,6 +14,7 @@ namespace _1.Scripts.DOTS.System
         public void OnCreate(ref SystemState state)
         {
             state.RequireForUpdate<SampleSpawnData>();
+            state.RequireForUpdate<SamplePMoveSpawnData>();
             state.RequireForUpdate<MapTileAuthoringComponentData>();
             state.RequireForUpdate<MapMakerComponentData>();
         }
