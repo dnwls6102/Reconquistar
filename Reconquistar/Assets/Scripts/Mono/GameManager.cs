3de4c65fa00ca9bb4b1590eab7efeab841e6b2ef
Hyun <jaehyun6687@naver.com>
2024-05-06 15:12:00 +0900

    Round & Occupy
    - 라운드 구현
    - 라운드 시작 전, 소유 땅 개수 적은 사람부터 순서대로 턴 가지도록
    - 땅 점령 구현

--- Reconquistar/Assets/Scripts/Mono/GameManager.cs
+++ Reconquistar/Assets/Scripts/Mono/GameManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Entities.UniversalDelegates;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,10 +12,14 @@ public class GameManager : MonoBehaviour
 
     private int playerNum = 4;
     private int currentPlayer;
+    private int currentTurn;
+
     public static bool isRolled; // 주사위 굴렸는지
     public static bool isMoving; // 이동 중인지
+
     private Player[] players;
-    private int[] tilePerPlayer; // player 소유한 타일 수
+    private Dictionary<int, int> tilePerPlayer; // player 소유한 타일 수
+    private List<int> playerTurnOrder = new List<int>();
 
     [SerializeField] private Button ClockwiseBtn;
     [SerializeField] private Button CounterClockwiseBtn;
@@ -21,17 +27,17 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         InitializePlayers();
-        currentPlayer = 0;
         isRolled = false;
         isMoving = false;
-        players[currentPlayer].SetArrow(true);
+
+        SortPlayerTurnOrder();
     }
 
     // player 생성, 소유 타일 수 지정
     private void InitializePlayers()
     {
         players = new Player[playerNum];
-        tilePerPlayer = new int[playerNum];
+        tilePerPlayer = new Dictionary<int, int>();
 
         for (int i = 0; i < playerNum; i++)
         {
@@ -39,7 +45,7 @@ public class GameManager : MonoBehaviour
             Player player = playerObject.GetComponent<Player>();
             player.SetCellIndex(i * GameBoard.tilePerLine);
             players[i] = player;
-            tilePerPlayer[i] = GameBoard.tilePerLine;
+            tilePerPlayer.Add(i, GameBoard.tilePerLine);
         }
     }
 
@@ -51,6 +57,23 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // 라운드 시작 전 플레이어 순서 정렬
+    private void SortPlayerTurnOrder()
+    {
+        tilePerPlayer = tilePerPlayer.OrderBy(item => item.Value).ThenBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+        playerTurnOrder.Clear();
+        foreach (KeyValuePair<int, int> player in tilePerPlayer)
+        {
+            playerTurnOrder.Add(player.Key);
+        }
+        Debug.Log("라운드 순서: " + String.Join(", ", playerTurnOrder));
+
+        currentPlayer = playerTurnOrder[0];
+        players[currentPlayer].SetArrow(true);
+        currentTurn = 0;
+    }
+
     // 이동 버튼 클릭
     public void ArrowBtnClick(bool clockwise)
     {
@@ -59,7 +82,7 @@ public class GameManager : MonoBehaviour
             Debug.Log("이동 중이거나 주사위를 굴리지 않았습니다.");
             return;
         }
-        
+
         isMoving = true;
         StartCoroutine(PlayerTurn(clockwise));
     }
@@ -71,6 +94,7 @@ public class GameManager : MonoBehaviour
 
         isMoving = false;
         isRolled = false;
+        OccupyRegion(); // 나중에 지워
 
         NextTurn();
     }
@@ -78,7 +102,32 @@ public class GameManager : MonoBehaviour
     private void NextTurn()
     {
         players[currentPlayer].SetArrow(false);
-        currentPlayer = (currentPlayer + 1) % playerNum;
-        players[currentPlayer].SetArrow(true);
+        currentTurn++;
+        
+        // 모두 턴 가졌으면 다음 라운드
+        if (currentTurn == playerNum) SortPlayerTurnOrder();
+        else
+        {
+            currentPlayer = playerTurnOrder[currentTurn];
+            players[currentPlayer].SetArrow(true);
+        }
+    }
+
+    // 점령 버튼 클릭 시 작동 - 지금은 도착만 해도 작동
+    public void OccupyRegion()
+    {
+        // 싸우는 스크립트 넣는 곳
+
+        bool win = true;
+        int currentCellIndex = players[currentPlayer].GetCellIndex();
+        MapTile currentMapTile = GameBoard.mapTiles[currentCellIndex];
+        if (win)
+        {
+            int currentOwner = currentMapTile.Owner;
+            currentMapTile.ChangeOwner(currentPlayer);
+
+            tilePerPlayer[currentOwner]--;
+            tilePerPlayer[currentPlayer]++;
+        }
     }
 }
